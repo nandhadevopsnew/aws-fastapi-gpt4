7eefafed49aaa3eaa95b6cf1a5859aa0ef5248c1
nandhadevopsnew <146066347+nandhadevopsnew@users.noreply.github.com>
2025-02-22 15:33:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -25,4 +25,4 @@ async def get_sentiment(input_text: InputText):
 
 @app.get("/")
 def read_root():
-    return {"message": "Hello MR.Nandha Demo project"}
+    return {"message": "Hello MR.Nandha Demo project workflow test"}
